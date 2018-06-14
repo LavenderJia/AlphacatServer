6c5d6fa6f1b7cb77bdcf717a1d01f7da17a1c64a
161250102 <161250102@smail.nju.edu.cn>
2018-06-14 15:04:14 +0800

    Get pic url list

--- src/main/java/com/alphacat/task/PictureController.java
+++ src/main/java/com/alphacat/task/PictureController.java
@@ -2,6 +2,7 @@ package com.alphacat.task;
 
 import com.alibaba.fastjson.JSON;
 import com.alphacat.service.PictureService;
+import com.alphacat.vo.PictureVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,6 +24,22 @@ public class PictureController {
     @PostMapping("/{taskId}")
     public void save(@PathVariable("taskId") int taskId, @RequestParam("file")MultipartFile file, @RequestParam("picIndex")int picIndex) {
         pictureService.uploadPic(file, taskId, picIndex);
+    }
+
+    @GetMapping("/{taskId}")
+    public Object getPicURL(@PathVariable("taskId") int taskId) {
+        File baseDir = new File("C:\\AlphaCatPic\\" + taskId);
+        File[] files = baseDir.listFiles();
+        PictureVO[] pictures = new PictureVO[files.length];
+        for (int i = 0; i < pictures.length; i++) {
+            String fileName = files[i].getName();
+            int dot = fileName.indexOf('.');
+            PictureVO picture = new PictureVO();
+            picture.setName(fileName);
+            picture.setUrl("http://localhost:8080/pic/"+taskId+"/"+fileName.substring(0, dot));
+            pictures[i] = picture;
+        }
+        return pictures;
     }
 
     @GetMapping("/{taskId}/{picIndex}")
---
+++ src/main/java/com/alphacat/vo/PictureVO.java
@@ -0,0 +1,15 @@
+package com.alphacat.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PictureVO {
+
+    private String name;
+    private String url;
+
+}
