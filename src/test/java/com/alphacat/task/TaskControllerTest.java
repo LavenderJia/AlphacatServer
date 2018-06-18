bb07684b17bd425eb8a1e89021ff88ac5d7c3fb9
UsagiHao <506887402@qq.com>
2018-06-18 19:09:27 +0800

    test for taskController

--- src/test/java/com/alphacat/task/TaskControllerTest.java
+++ src/test/java/com/alphacat/task/TaskControllerTest.java
@@ -4,14 +4,10 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alphacat.mapper.TaskMapper;
 import com.alphacat.mapper.LabelMapper;
-import com.alphacat.pojo.Requester;
 import com.alphacat.pojo.Task;
-import com.alphacat.vo.RequesterVO;
+import com.alphacat.vo.AvailableTaskVO;
 import com.alphacat.vo.TaskVO;
 import com.alphacat.vo.LabelVO;
-//import com.alphacat.vo.W_TaskVO;
-//import com.alphacat.vo.R_TaskVO;
-import com.alphacat.vo.HistoryTaskVO;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -24,14 +20,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -64,11 +61,11 @@ public class TaskControllerTest {
     private List<LabelVO> labelB = new ArrayList<LabelVO>( );
     private List<LabelVO> labelD = new ArrayList<LabelVO>( );
 
-    TaskVO taskA=new TaskVO(1,1,"t11","aaa",1,10,2,false,"2018-04-27","2018-05-01",null);
-    TaskVO taskB=new TaskVO(2,1,"t12","bbb",2,20,1,true,"2018-04-01","2018-04-10", labelB);
-    TaskVO taskC=new TaskVO(3,2,"t23","ccc",3,30,3,false,"2018-04-29","2018-05-10",null);
-    TaskVO taskD=new TaskVO(4,2,"t24","ddd",4,40,1,false,"2018-04-20","2018-05-20",labelD);
-    TaskVO taskE=new TaskVO(5,1,"t15","eee",5,50,2,true,"2018-04-10","2018-04-27",null);
+    TaskVO taskA=new TaskVO(1,0,"测试任务一","新手任务——单框标注测试任务",2,20,1,false,"2018-04-01","2019-04-10",null);
+    TaskVO taskB=new TaskVO(2,0,"测试任务二","新手任务——多框标注测试任务",2,20,2,false,"2018-06-01","2019-06-30", labelB);
+    TaskVO taskC=new TaskVO(3,0,"测试任务三","新手任务——不规则标注测试任务",4,30,3,false,"2018-05-05","2019-05-16",null);
+    TaskVO taskD=new TaskVO(4,2,"t24","ddd",4,40,1,false,"2018-05-30","2018-06-20",labelD);
+    TaskVO taskE=new TaskVO(5,1,"t15","eee",5,50,2,true,"2018-05-30","2018-06-27",labelD);
 
 
     @Before
@@ -97,44 +94,49 @@ public class TaskControllerTest {
      * A test for add a single task account with its password.
      */
     private void testSingleAdd(TaskVO task) throws Exception {
+        int id = task.getId();
         JSONObject r = (JSONObject) JSON.parse(JSON.toJSONString(task));
-        r.fluentRemove("label");
+        r.fluentRemove("id").fluentPut("state", "draft");
         request = post("/task")
-                .contentType(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
                 .content(r.toJSONString());
         mvc.perform(request).andExpect(status().isOk());
         // test for its existence
-        Task actual = taskMapper.get(4);
+        Task actual = taskMapper.get(id);
         Task expected = taskConverter.toPOJO(task);
         assertEquals(expected, actual);
     }
 
-/*    @Test
+    @Test
     public void B_updateTask() throws Exception{
-        LabelVO[] labelsD=new LabelVO[]{labelD1,labelD2};
-        taskD.setLabels(labelsD);
-        request = post("/task/update")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(JSONObject.toJSONString(taskD));
+        labelD.add(labelD1);
+        labelD.add(labelD2);
+        TaskVO newTaskD=new TaskVO(4,2,"t25","ddd",4,40,1,false,"2018-06-19","2018-06-20",labelD);
+        JSONObject r = (JSONObject) JSON.parse(JSON.toJSONString(newTaskD));
+        request = put("/task")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(r.toJSONString());
         mvc.perform(request).andExpect(status().isOk());
+        assertEquals("t25",taskMapper.get(4).getName());
     }
 
     @Test
-    public void C_getPublishedTasks() throws Exception{
-        List<R_TaskVO> result=new LinkedList<>();
-        result.add(new R_TaskVO(1,"t11","2018-04-27","2018-05-01",1,0,0.0));
-        result.add(new R_TaskVO(5,"t15","2018-04-10","2018-04-27",1,0,0.0));
-        request = post("/task/published")
-                .param("requesterId","1");
-        mvc.perform(request).andExpect(content().json(JSONObject.toJSON(result).toString()));
+    public void C_getIdleTasks() throws Exception{
+        AvailableTaskVO idleTaskE = new AvailableTaskVO(10,"t25", 5,50,2, "2018-06-27");
+        List<AvailableTaskVO> result=new ArrayList<>();
+        result.add(idleTaskE);
+        request = get("/task?requesterId=1&type=underway");
+        mvc.perform(request).andExpect(status().isOk())
+                .andExpect(content().json(JSONObject.toJSON(result).toString()));
     }
 
-    @Test
-    public void C_getFinishedTasks() throws Exception{
+ /*   @Test
+    public void C_getIdleTasks() throws Exception{
         List<R_TaskVO> result=new LinkedList<>();
         result.add(new R_TaskVO(2,"t12","2018-04-01","2018-04-10",2,3,1.2));
-        request = post("/task/finished")
-                .param("requesterId","1");
+        request = get("/task")
+                .param("requesterId","1")
+                .param("type", "notstart");
         mvc.perform(request).andExpect(content().json(JSONObject.toJSON(result).toString()));
     }
 
